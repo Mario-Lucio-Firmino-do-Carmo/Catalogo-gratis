5604d10dcc96c819e6a89b319c62e82fb9ad57c8
Mário Lúcio Firmino do Carmo <152232845+Mario-Lucio-Firmino-do-Carmo@users.noreply.github.com>
2025-07-18 22:02:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,35 @@
+/**
+ * ====================================================================
+ * CONFIGURAÇÃO DO CARDÁPIO DIGITAL - V3.2 - 17/07/2025
+ * ====================================================================
+ * 
+ * Este arquivo contém todas as configurações principais do sistema,
+ * incluindo URLs das planilhas e textos traduzíveis.
+ */
+
+// ====================================================================
+// URLs DAS PLANILHAS DO GOOGLE SHEETS
+// ====================================================================
+/**
+ * Para alterar as planilhas utilizadas pelo cardápio, modifique as URLs abaixo.
+ * Certifique-se de que as planilhas estejam publicadas e acessíveis publicamente.
+ * 
+ * Como obter a URL correta:
+ * 1. Abra sua planilha no Google Sheets
+ * 2. Vá em Arquivo > Publicar na web
+ * 3. Selecione a aba específica que deseja publicar
+ * 4. Escolha o formato CSV
+ * 5. Copie a URL gerada
+ */
+
+// URL of the spreadsheet with menu items (tab: Items)
+const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRJw1Uy32kypDVasSCGW8K1gQd8y6c47qvlC2QHEWNRC8ys30wTD1Licd0HQBSbIPiCr4Kolmawm01G/pub?gid=0&single=true&output=csv';
+
+// URL of the spreadsheet with menu categories (tab: Categories)
+const CATEGORIES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRJw1Uy32kypDVasSCGW8K1gQd8y6c47qvlC2QHEWNRC8ys30wTD1Licd0HQBSbIPiCr4Kolmawm01G/pub?gid=1820214392&single=true&output=csv';  
+
+// URL of the spreadsheet with operating hours (tab: Hours)
+const HOURS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRJw1Uy32kypDVasSCGW8K1gQd8y6c47qvlC2QHEWNRC8ys30wTD1Licd0HQBSbIPiCr4Kolmawm01G/pub?gid=2058376136&single=true&output=csv';
+
+// URL of the spreadsheet with general settings (tab: Config)
+const CONFIG_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRJw1Uy32kypDVasSCGW8K1gQd8y6c47qvlC2QHEWNRC8ys30wTD1Licd0HQBSbIPiCr4Kolmawm01G/pub?gid=1780915110&single=true&output=csv';
